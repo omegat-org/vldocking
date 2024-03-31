fb6c828334483486ded3578a1cf5704632ba3f6d
Hiroshi Miura <miurahr@linux.com>
2024-03-31 11:25:54 +0900

    chore: add copyright header for new files

--- src/main/java/com/vlsolutions/swing/docking/SplitResizer.java
+++ src/main/java/com/vlsolutions/swing/docking/SplitResizer.java
@@ -1,3 +1,20 @@
+/*
+    VLDocking Framework 3.0
+    Copyright Lilian Chamontin, 2004-2013
+
+    www.vldocking.com
+    vldocking@googlegroups.com
+------------------------------------------------------------------------
+This software is distributed under the LGPL license
+
+The fact that you are presently reading this and using this class means that you have had
+knowledge of the LGPL license and that you accept its terms.
+
+You can read the complete license here :
+
+    http://www.gnu.org/licenses/lgpl.html
+
+*/
 package com.vlsolutions.swing.docking;
 
 import javax.swing.*;
