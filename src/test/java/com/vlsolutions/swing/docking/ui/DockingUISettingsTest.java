2a838b7248bab6ac769a11914fe36e0820f74b3f
Hiroshi Miura <miurahr@linux.com>
2024-09-24 18:17:55 +0900

    chore: add DockingUISettingsTest

---
+++ src/test/java/com/vlsolutions/swing/docking/ui/DockingUISettingsTest.java
@@ -0,0 +1,36 @@
+package com.vlsolutions.swing.docking.ui;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.swing.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class DockingUISettingsTest {
+
+    private static int notificationBlinkCount = 2;
+
+    @Before
+    public void setUp() {
+        DockingUISettings.setInstance(new CustomDockingUISettings());
+    }
+
+    @Test
+    public void testCustomizedParameters() {
+        DockingUISettings.getInstance().installUI();
+        assertEquals(notificationBlinkCount, UIManager.get("DockingDesktop.notificationBlinkCount"));
+        notificationBlinkCount = 3;
+        DockingUISettings.getInstance().updateUI();
+        assertEquals(notificationBlinkCount, UIManager.get("DockingDesktop.notificationBlinkCount"));
+    }
+
+    public static class CustomDockingUISettings extends DockingUISettings {
+
+        @Override
+        protected UIDefaults getDefaults(UIDefaults table) {
+            table.put("DockingDesktop.notificationBlinkCount", notificationBlinkCount);
+            return super.getDefaults(table);
+        }
+    }
+}
